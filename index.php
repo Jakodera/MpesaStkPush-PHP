83a4240f3ed9bca6ebc1662709fd22b851dd859a
Jakodera <you@example.com>
2021-09-19 10:25:03 +0300

    Added CSS

--- index.php
+++ index.php
@@ -3,23 +3,20 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>skypesa | MPESA Payment</title>
-    <link rel="stylesheet" href="assets/payment.css" />
+    <title>Jakodera | MPESA Payment</title>
+    <link rel="stylesheet" href="payment.css" />
 </head>
 
 <body>
     <div class="card-container">
-        <div class="top">
-            <a href="index.php"><img src="assets/mpesa-logo.png" width="100%" alt=""></a>
-        </div>
         <div class="middle">
-            <h3>Payment Details</h3>
+            <h3>MPESA STK PUSH </h3>
             <div class="extra-detail">
                 <div class="detail">
-                    <p id="dark">Total Amount</p>
+                    <p id="dark">Amount to Pay</p>
                 </div>
                 <div class="detail-price">
-                    <p id="dark">KES 1</p>
+                    <p id="dark">Ksh. 1</p>
                 </div>
             </div>
             <div class="border"></div>
@@ -36,7 +33,6 @@
 
                             2. Check your mobile phone for a prompt asking to enter M-PESA pin.
 
-
                         </p>
                     </div>
 
@@ -50,11 +46,11 @@
                 <div class="name-area">
                     <form class="ajaxx" action="ajax/getters.php" method="POST">
                         <div class="name">
-                            <h3>Enter phone number for payment here:</h3>
+                            <h3>Phone:</h3>
                             <input type="text" name="phone" placeholder="0700000000" class="input-no" required />
                         </div>
                         <div class="payment-buttons">
-                            <input type="submit" name="submit" value="Make Payment" class="pay-btn">
+                            <input type="submit" name="submit" value="Pay" class="pay-btn">
                         </div>
                     </form>
 
